32021abae53b0b47ae940b057b013f28ff0e9bac
Jhon Muller <jmuller.jhon80@gmail.com>
2022-11-17 17:31:38 -0400

    feat: estruturação do displaycomponent.

--- anticipation-calculator/src/Pages/Home/Home.jsx
+++ anticipation-calculator/src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import DisplayComponent from "../../Components/Display/DisplayComponent";
 import FormComponent from "../../Components/Form/FormComponent";
 import "./styles.css"
-
+import { useState } from "react";
 const Home = () => {
+
+    const [data, setData] = useState([])
+    console.log(data)
+
     return(
         <div className="container-home">
-            <FormComponent/>
-            <DisplayComponent/>
+            <FormComponent setData={setData} />
+            <DisplayComponent />
         </div>
     )
 }
